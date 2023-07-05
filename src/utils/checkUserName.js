3e2b10fd663c9b76138632e8518cfe1766212682
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2023-07-05 12:58:05 +0300

    feat: correct my solution after finish crosscheck

--- src/utils/checkUserName.js
+++ src/utils/checkUserName.js
@@ -1,5 +1,6 @@
 export const checkUser = () =>{
   const args = process.argv.slice(2);
+  if (args.length === 0) return;
   if(args[0].includes('--username=') && args.length ===1 
   && args[0].split('=')[0]==='--username'&& args[0].split('=')[1].trim()) {
     return args[0].split('=')[1].trim();
